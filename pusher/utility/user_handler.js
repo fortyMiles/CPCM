ab167eadb0d123e86ce0667385561f25e8817617
fortymiles <mqgao@outlook.com>
2015-10-23 12:26:43 +0800

    Add use class some static method and variables. Could  let this class handle more things.

--- pusher/utility/user_handler.js
+++ pusher/utility/user_handler.js
@@ -7,42 +7,56 @@
 
 module.exports = User;
 
+/* define User static variables */
+
+User.on_line_person = 0;
+User.socket_user = {socket_id: null, user_name: null};
+User.login_users = {}; // formate {name:varchar()}, to save all the logined user;
+User.user_info = {socket:null, socket_id:null};
+User.unread_messages = {};  //unread_messages = {'username':message_list};
+
+/* define User static variable end */
+
+/* define User static method.*/
+
+User.get_socket_by_name = function(user_name){
+	var user_info = User.login_users[user_name];
+	return User.login_users[user_name].socket;
+};
+
+/* define User static method end.*/
+
 function User(user_name, socket, socket_id){
 
-	var	socket_user = {socket_id: null, user_name: null},
-		logined_users = {}, // formate {name:varchar()}, to save all the logined user.
-		user_info = {socket:null, socket_id:null};
-		unread_messages = {}, // unread_messages = {'username':message_list};
-		// some message send to a user, but this user not logined when send to him.
-		// wait_messages = {name:message_info};
-		// 
-		
 	this.user_name = user_name;
 	this.socket = socket;
 	this.socket_id = socket_id;
 
-
-
 	this.test = function(){
 		console.log('hello ~');
 		console.log('hi i am ' + this.user_name);
+		console.log('value == ' + User.on_line_person);
+		User.on_line_person++;
 	};
 
 	this.login = function(){
 		/*
 		 * add user information to loggin list.
 		 */
-		var user_info = {socket: this.user_socket, socket_id: this.user_soccket_id};
+		var user_info = {socket: this.socket, socket_id: this.socket_id};
 		
-		logined_users[this.user_name] = user_info;
-		socket_user[this.user_socket] = this.user_name;
+		User.login_users[this.user_name] = user_info;
+		console.log(User.login_users);
+		User.socket_user[this.user_socket] = this.user_name;
+		console.log('read test');
+		console.log(User.socket_user[this.user_socket]);
 	};
 
 	this.have_unread_message = function(){
 		/*
 		 * to check this user whether have unread_message.
 		 */
-		return this.user_name in unread_messages;
+		return this.user_name in User.unread_messages;
 	};
 
 	this.get_unread_messages = function(){
@@ -57,14 +71,14 @@ function User(user_name, socket, socket_id){
 		 *  returns:
 		 *    the unread message list.
 		 */
-		return unread_messages[this.user_name];
+		return User.unread_messages[this.user_name];
 	};
 
 	this.is_login = function(){
 		/*
 		 * test user if logged in.
 		 */
-		return this.user_name in logined_users;
+		return this.user_name in User.login_users;
 	};
 
 	this.add_one_unread_message = function(message){
@@ -73,13 +87,13 @@ function User(user_name, socket, socket_id){
 		 */
 		
 		if(!this.have_unread_message()){
-			unread_messages[this.user_name] = [];
+			User.unread_messages[this.user_name] = [];
 		}
 
-		unread_messages[this.user_name].push(message);
+		User.unread_messages[this.user_name].push(message);
 	};
 
-	this.get_user_socket = function(user_name){
+	var get_user_socket = function(user_name){
 		/*
 		 * gives a socket by user name;
 		 * args:
@@ -89,22 +103,43 @@ function User(user_name, socket, socket_id){
 		 *	socket: this socket bind with this user.
 		 *
 		 */
-		return logined_users.user_name.socket;
+		return User.login_users.user_name.socket;
 	};
 
-	this.delte_by_socket = function(socket){
-		// delete one user information by a socket.
-		// args:
-		//	socket: the need to delete user's socket.
-		// returns:
-		//  delete: Boolean, if delete successfully.
+	this.logout = function(socket){
+		return delete User.login_users[this.user_name];
+	}
+
+	this.disconnect = function(socket){
+
+		/*
+		 * one socket disconnect.
+		 * delete one user information by a socket.
+		 * ----
+		 *	args:
+		 *		socket: the need to delete user's socket.
+		 *	returns:
+		 *		delete: Boolean, if delete successfully.
+		 */
+		
+		var user_name = User.socket_user[socket];
+		return delete User.login_users[user_name];
 	};
 }
 
-var user = new User('minchiuan', 12, 111);
+
+var user = new User('minchiuan', 'sokcet', 111);
 user.login();
+user.test();
 console.log(user.is_login());
 console.log(user.add_one_unread_message(' message 0'));
 console.log(user.add_one_unread_message(' message 1'));
 console.log(user.have_unread_message());
 console.log(user.get_unread_messages());
+console.log(User.value);
+
+
+var new_user = new User('mike', 'socket 2', 122);
+new_user.test();
+
+console.info(User.get_socket_by_name('minchiuan'));
