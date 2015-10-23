e933e9e970ad2a42cbce46a45e1bde092229cb8f
fortymiles <mqgao@outlook.com>
2015-10-23 10:01:19 +0800

    change server to module

--- pusher/server.js
+++ pusher/server.js
@@ -1,58 +1,69 @@
 var io = require('socket.io'),
-	ioServer = io.listen(3000),
-	sequence = 1,
-	clients = [];
+	ioServer = io.listen(3000);
 
-var CONNECTION = 'connection',
-	DISCONNECT = 'disconnect',
-	CHAT_MESSAGE = 'chat message',
-	ADD_USER = 'add user',
-	INFORMATION_RECEIVED = 'information received',
-	ERROR = 'error';
+var controller_handler = require('./utility/controller_handler.js');
+var user_handler = require('./utility/user_handler.js');
+var events = new require('./configration/status.js');
+var e = new events();
 
-ioServer.on(CONNECTION, function(socket) {
+console.log(e.CONNECTION);
+var user = new user_handler();
+user.test();
+
+ioServer.on(e.CONNECTION, function(socket) {
 	console.info('New client connected (id=' + socket.id + ').');
+
+	socket.on(LOGIN, function(msg){
+		// msg = {'name':varchar}
+		console.info('a new user login');
+		msg = JSON.parse(msg);
+		user_name = msg.name.trim();
+
+		user.add_a_login_user(username=user_name, socket=socket, user_socket_id=socket_id, function(){
+			addUserMessage = {'newUser':user_name};
+			ioServer.emit(ADD_USER, user_name);
+			// after added, broadcast to every that added a new user;
+		});
+	});
+
 	socket.on(CHAT_MESSAGE, function(msg){
 		console.info(msg);
 		msg = JSON.parse(msg);
-		console.info(msg['sender']);
-		console.info(msg['receiver']);
-		console.info(msg['message']);
-		sender = msg['sender'].trim();
-		receiver = msg['receiver'].trim();
-		content = msg['message'];
+		sender = msg.sender.trim();
+		receiver = msg.receiver.trim();
+		message = msg.message;
 
-		socket.emit(CHAT_MESSAGE, "send success");
+		socket.emit(SENT_TO_SERVER,'');
+		
+		// test code
 		console.log('send message to sender');
-		console.log(sender + ' send message' + 'and he said ' + content);
+		console.log(sender + ' send message' + 'and he said ' + message);
 
-		if(sender != 'none'){
-			var user_info = {name:sender, socket:socket, socket_id: socket.id};
-			console.log('***********add a new user');
-			clients.push(user_info);
-
-			addUserMessage = {'newUser':sender};
-			ioServer.emit(ADD_USER, addUserMessage);
-		}
-
-		if(receiver != 'none'){
-			console.log('receiver is not null');
+		if(sender && receiver){
 			if(receiver == 'all'){
-				//ioServer.emit('chat message', msg.content); // will send to self.
-				socket.broadcast.emit('chat message', msg);// don's send to self
+				socket.broadcast.emit(CHAT_MESSAGE, msg);// don's send to self
 			}else{
-				var receiver = get_person(receiver);
-				if(receiver){
-					console.log('find a receiver in list');
-					receiver.emit('chat message',msg);
-					socket.emit('chat message', {'message':'send success'});
+				if(user.is_login(receiver)){
+					var receiver_socket = user.get_socket_by_user(receiver);
+					receiver_socket.emit(CHAT_MESSAGE, msg);
+
+					socket.emit(SENT_TO_RECEIVER, '');// send to client, his message it sent to receiver successfully.
+				}else{
+					var unread_message = {sender:null, message:null, date:null};
+					unread_message.receiver = sender;
+					unread_message.message = message;
+					unread_message.date = new Date();
+
+					user.add_to_unread_list(receiver, unread_message);
 				}
 			}
-
-			socket.emit('information been received', {'message':'reveiced'});
-			console.info('reveive message from ' + sender);
 		}
+	};
+
+	socket.on(INVITATION, function(){
+		// invitaion one person.
 	});
+
 
 	socket.on(DISCONNECT, function() {
 		var user_index = get_user_by_socket(socket);
@@ -90,3 +101,4 @@ function get_user_by_socket(socket){
 	}
 	return user_index;
 }
+*/
