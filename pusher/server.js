45cbad148e3e2c3a53faaad839b44697059ab3f4
fortymiles <mqgao@outlook.com>
2015-10-30 15:56:58 +0800

    1. Split the send_message() out. 2. Change receive mes format to be msg.data ..

--- pusher/server.js
+++ pusher/server.js
@@ -12,11 +12,11 @@ ioServer.on(e.CONNECTION, function(socket) {
     console.info('New client connected (id=' + socket.id + ').');
 
     socket.on(e.CHAT_MESSAGE, function(msg){
-        //msg = JSON.parse(msg);
+        msg = JSON.parse(msg);
         console.log(msg);
         var type = msg.type.trim();
-        var sender = msg.from.trim();
-        var receiver = msg.to.trim();
+        var sender = msg.data.from.trim();
+        var receiver = msg.data.to.trim();
 
         switch(type){
             case 'login':
@@ -40,34 +40,60 @@ ioServer.on(e.CONNECTION, function(socket) {
                 break;
 
             case 'chat':
-                console.info(msg);
-                message = msg.message;
+                msg.data.date = get_date();
+                send_message(sender, receiver, msg);
+                break;
 
-                // test code
-                console.log('send message to sender');
-                console.log(sender + ' send message' + 'and he said ' + message);
-
-                if(sender && receiver){
-         //           msg = JSON.stringify(msg);
-                    console.log(User.is_login(receiver));
-                    console.log(User.is_login(sender));
-                    if(receiver == 'all'){
-                        socket.broadcast.emit(CHAT_MESSAGE, msg);// don's send to self
-                    }else{
-                        if(User.is_login(receiver)){
-                            console.log('receiver is login');
-                            var receiver_socket = User.get_socket_by_name(receiver);
-                            receiver_socket.emit(e.CHAT_MESSAGE, msg);
-                            socket.emit(e. CHAT_MESSAGE, "you said: " + msg);
-                        }else{
-                            socket.emit(e. CHAT_MESSAGE, "receiver is not login: " + msg);
-                            User.add_one_unread_message(receiver, msg);
-                        }
-                    }
+            case 'invitation':
+                if(!User.is_registerred(receiver)){
+                    // send phone message to receiver
                 }
+                msg.data.date = get_date();
+                send_message(sender, receiver, msg);
+                
+                var relation = msg.data.relation;
+
+                // create the two person relation.
+
+                
+                break;
+            case 'accept':
                 break;
         }
     });
+    
+    var get_date = function(){
+        var date = new Date();
+        return date;
+    };
+
+    var send_message = function(sender, receiver, msg){
+        console.info(msg);
+        message = msg.data.message;
+
+        // test code
+        console.log('send message to sender');
+        console.log(sender + ' send message' + 'and he said ' + message);
+
+        if(sender && receiver){
+            msg = JSON.stringify(msg);
+            console.log(User.is_login(receiver));
+            console.log(User.is_login(sender));
+            if(receiver == 'all'){
+                socket.broadcast.emit(CHAT_MESSAGE, msg);// don's send to self
+            }else{
+                if(User.is_login(receiver)){
+                    console.log('receiver is login');
+                    var receiver_socket = User.get_socket_by_name(receiver);
+                    receiver_socket.emit(e.CHAT_MESSAGE, msg);
+                    socket.emit(e. CHAT_MESSAGE, "you said: " + msg);
+                }else{
+                    socket.emit(e. CHAT_MESSAGE, "receiver is not login: " + msg);
+                    User.add_one_unread_message(receiver, msg);
+                }
+            }
+        }
+    };
 
 
     socket.on(e.DISCONNECT, function() {
