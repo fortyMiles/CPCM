b186f2784cda7a2eff5df650de74a380648a2f27
fortymiles <mqgao@outlook.com>
2015-11-02 16:52:58 +0800

    add receiver

---
+++ pusher/receiver.js
@@ -0,0 +1,9 @@
+/*
+ * Receiver, a server that listen the clients's messages, 
+ * and save it very quickly to a database.
+ * 
+ * Author: Minchiuan.Gao <minchiuan.gao@gmail.com>
+ * Date: 2015-Nov-2
+ * 
+ */
+
