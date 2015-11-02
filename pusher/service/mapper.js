84420990a27946a2ad01bbfefec89e18a4570a79
fortymiles <mqgao@outlook.com>
2015-11-02 21:32:10 +0800

    implements the RECEIVER

--- pusher/service/mapper.js
+++ pusher/service/mapper.js
@@ -6,4 +6,13 @@
  *
  */
 
-var insert_sql = "INSERT INTO MESSAGE SET ?"
+module.exports = Mapper
+
+var status = require('./Configuration.js');
+var c = new status();
+
+function Mapper(){
+    this.insert = "INSERT INTO message SET ?";
+    this.select_eariliest = "select * from message.message where status = 'U' ORDER BY create_date LIMIT 1";
+    this.set_to_read = "update message set status = '" + c.read + "' where id = ?";
+}
