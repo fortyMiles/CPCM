41ef5d1a5742e2d81e4fbe9d400484a630a512c5
fortymiles <mqgao@outlook.com>
2015-11-04 19:26:41 +0800

    parse client in client js

---
+++ client/static/send_message.js
@@ -0,0 +1,21 @@
+var CHAT = 'chat message';
+console.log('import send_message.js success');
+var socket = io('http://localhost:3000');
+$('form').submit(function(){
+	var message = $('#message').val();
+
+	console.log('message == ' + message);
+        var json_data = JSON.parse(message);
+	socket.emit(CHAT, json_data);
+	$('#message').val('');
+	return false;
+});
+
+socket.on(CHAT, function(msg){
+        var message = JSON.stringify(msg);
+	$('#messages').append($('<li>').text(message));
+});
+socket.on('add user', function(msg){
+	$('#users').append($('<li>').text('exised user : ' + msg));
+});
+
