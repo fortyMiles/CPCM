9594a218b0b8fc599d8503ae7319bc3c066819c2
fortymiles <mqgao@outlook.com>
2015-11-05 12:02:51 +0800

    change server js name to main.js

--- pusher/main.js
+++ pusher/main.js
@@ -1,138 +1,75 @@
 /*
- * Change server to single event.
- * Author: Minchiuan Gao <mimchiuan.gao@gmail.com>
- * Date: 2015-Oct-29
+ * Receiver, a server that listen the clients's messages, 
+ * and save it very quickly to a database.
+ * 
+ * Author: Minchiuan.Gao <minchiuan.gao@gmail.com>
+ * Date: 2015-Nov-2
+ * 
  */
 
-function MainServer(port){
 
-    var io = require('socket.io'),
-    events = new require('./configration/status.js');
 
-    var e = new events(),
-    User = require('./utility/user_handler.js');
+var io = require('socket.io'),
+    io_server = io.listen(3000);
 
-    var Message = require('./configration/message.js'),
-    m = new Message();
+var events = new require('./configration/events.js'),
+    e = new events();
 
-    this.io_server = io.listen(port);
-    this.socket = null;
+var UserService = require('./service/user_service.js'),
+    user_service = new UserService();
 
-    var message = {type:null, sender:null, receiver:null, message:null};
+var MessageService = require('./service/message_service.js'),
+    message_service = new MessageService();
 
-    this.events_handler = function(socket){
-        console.info('a new client connects you, ( id == ' + socket.id + ' )');
-        socket.emit(e.CHAT_MESSAGE, {'message':'you are connected!'});
+var SocketService = require('./service/socket_service.js'),
+    socket_service = new SocketService();
 
-        socket.on(e.CHAT_MESSAGE, function(msg){
-            var type = msg.type.trim();
-            switch(type){
-                case e.LOGIN:
-                    user = new User(msg.name.trim(), socket, socket.id);
-                    user.login();
-                    send_ensure_message(msg.name.trim(), socket);
-                    send_unread_message(user);
-                    break;
+io_server.on(e.CONNECTION, function(socket) {
+    console.info('New client connected (id=' + socket.id + ').');
 
-                case e. INVITATION:
-                    var sender = msg.from.trim();
-                    var receiver = msg.to.trim();
-                    var nickname = msg.nickname.trim();
-                    var speak = 'hi, ' + nickname + ' ' + ' wo 是' + sender + ' 一起加入麦粒吧~ ';
-                    var message = {type: "invitation", from: sender, to: receiver, message: speak};
-                    if(User.is_registerred(receiver)){
-                        if(User.is_login(sender) && User.is_login(receiver)){
-                            console.log(message);
-                            var destination_socket = User.get_socket_by_name(receiver);
-                            destination_socket.emit(e. CHAT_MESSAGE, message); // router the message to the receiver;
-                            var sender_socket = User.get_socket_by_name(sender);
-                            sender_socket.emit(e. CHAT_MESSAGE, {'type':'send success'});
-                        }
-                    } 
-                    break;
+    socket.on(e.CHAT_MESSAGE, function(msg){
+        var type = msg.type.trim();
+        console.log(type);
 
-                case e. CHAT:
-                    console.log('receiver a message');
-                    console.log(msg);
-                    var sender = msg.from.trim();
-                    var receiver = msg.to.trim();
-                    send_message(sender, receiver, msg);
-                    break;
-            }
-        });
-
-        socket.on(e.DISCONNECT, function(){
-            console.log('a user leave..');
-            User.disconnect(socket);
-        });
-    };
-
-    var send_unread_message = function(user, socket){
-        if(user.have_unread_message()){
-            console.log('has unread message');
-            var unread_messages = user.get_unread_messages();
-            var user_socket = User.get_user_socket(user.user_name);
-
-            for(var index = 0; index < unread_messages.length; index++){
-                user_socket.emit(unread_messages[index].events, unread_messages[index].message);
-            }
-            /* !!! notice, need to add a receive confire from client, it's just test code */
-            user.delete_unread_message();
+        if(type == e. LOGIN){
+            var username = msg.data.from.trim();
+            user_service.login_one_user(username, socket.id, socket, function(){
+                socket_service.registe_socket(username, socket, function(){
+                    socket_service.send_syn(socket, username);
+                });
+            });
         }
-    };
 
-    var send_ensure_message = function(name, socket){
-        // sends to name an ensure message
-        var send_message = {type: e.LOGIN, message: 'hello ' + name};
-        socket.emit(e.CHAT_MESSAGE, send_message); 
-    };
+        if(type == e.CHAT){
+            message_service.save_a_new_message(msg);
+        }
 
-    var login = function(name, socket){
-        console.info('namemadnwadkajw');
-        console.info(name);
-        return user;
-    };
+    });
 
-    var send_message = function(sender, receiver, msg){
-        /* sends a message from sender to receiver.
-         * 
-         * args:
-         *	sender: sender's account name
-         *	receiver: receiver's account name
-         *	message: message content
-         *	send_event: which events neeed send to receiver, such as 'chat message', 'invitation', etc
-         *	feedback_events: which events when send success give back to sender.
-         *
-         */
-        msg.server_date = new Date().getTime();
+    socket.on(e.FEED, function(msg){
+    });
 
-        if(User.is_login(sender) && User.is_login(receiver)){
-            console.log('both are logined');
-	    var destination_socket = User.get_socket_by_name(receiver);
-	    destination_socket.emit(e. CHAT_MESSAGE, msg); // router the message to the receiver;
-	    var sender_socket = User.get_socket_by_name(sender);
-            var feedback = {type:'send_success'};
-	    sender_socket.emit(e. CHAT_MESSAGE, feedback);
-	}else{
-	    if(!User.is_login(sender)){
-		console.log('user not login');
-	     }
-	        // add message to a receiver's unread message.
-	    if(!User.is_login(receiver)){
-                console.log('receier not log');
-	        User.add_one_unread_message(receiver, e. CHAT_MESSAGE, msg);
-	    }
-	}
-    };
+    /** check new message **/
+    socket.on('checker ready', function(){
+        socket.emit('check');
+    });
 
-    this.run = function(){
-        /* 
-         * run a new process to handle connection.
-         */
-        console.log('server start');
-        this.io_server.on(e.CONNECTION, this.events_handler);
-    };
-}
+    socket.on(e.NEW_MESSAGE, function(msg){
+        socket_service.emit_message(e. CHAT_MESSAGE, msg, function(id){
+            message_service.set_an_unread_message_to_read(id, function(){
+                socket.emit('check');
+            });
+        });
+        console.log('have new message');
+    });
+    /** check new message end **/
 
-var server = new MainServer(3000);
-server.run();
+    socket.on(e.DISCONNECT, function(socket){
+        user_service.disconnect(socket);
+    });
+
+    //socket_service.send_unread_messages();
+    //console.log('i get unread messages');
+    // this sectence will execute all the time ~, it's interestring :)
+});
+
