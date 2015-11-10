92f62344f5c95d9ac343aa2d2225abd986c66f4b
fortyMiles <mqgao@outlook.com>
2015-11-10 17:44:05 +0800

    see what is in the dic clients

---
+++ pusher/version2.js
@@ -0,0 +1,31 @@
+var io = require('socket.io'),
+    io_server = io.listen(2333);
+
+var clients = {};
+
+io_server.on('connection', function(socket){
+    console.info('New client connecte (id=' + socket.id + ').');
+
+	socket.on('login', function(msg){
+		if(clients[msg.data.from]){
+			clients[msg.data.from] = socket;
+		}
+		see_dic(clients);
+		io_server.emit('chat message', msg.data.from + ' on line now');
+	});
+
+	socket.on('chat message', function(msg){
+		if(msg.data.to == 'all'){
+			io_server.emit('chat message', msg);
+		}else{
+			see_dic(clients);
+			clients[msg.data.to].emit('chat message', msg);
+		}
+	});
+});
+
+function see_dic(dic){
+	for(var e in dict){
+		console.log(e);
+	}
+}
