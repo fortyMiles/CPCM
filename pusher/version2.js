07010b7c88e789c325b52fe33a90dbdf6b37e022
fortyMiles <mqgao@outlook.com>
2015-11-11 11:07:21 +0800

    reconstruct the source code. split to different functions. Achieved functions: 1. broadcast login users; 2. send messages to online. 3. find off-line users.

--- pusher/version2.js
+++ pusher/version2.js
@@ -1,46 +1,56 @@
 var io = require('socket.io'),
-    io_server = io.listen(2333);
+	io_server = io.listen(2333);
 
-var MessageService = require('./service/message_service.js'),
+var MessageService = require('./service/message_service.js'), 
 	message_service = new MessageService();
 
 var clients = {};
 
 io_server.on('connection', function(socket){
-    console.info('New client connecte (id=' + socket.id + ').');
+	console.info('New client connecte (id=' + socket.id + ').');
 
 	socket.on('login', function(msg){
-		if(check_fomat(msg)){
-			if(!clients[msg.data.from]){
-				clients[msg.data.from] = socket;
-			}
-			io_server.emit('chat message', msg.data.from + ' on line now');
-		}
+		msg_handler(msg, socket, _broadcast_login);
 	});
 
 	socket.on('chat message', function(msg){
-		if(check_fomat(msg)){
-			//if(!clients[msg.data.from])||(!clients[msg.data.from].connected)
-			if(msg.data.to == 'all'){
-				io_server.emit('chat message', msg);
-			}else{
-				if((msg.data.to in clients) && (clients[msg.data.to].connected)){
-					clients[msg.data.to].emit('chat message', msg);
-				}else{
-					message_service.save_a_new_message(msg);
-				}
-			}
-		}
+		msg_handler(msg, socket, _send_message);
 	});
+
 });
 
-function see_dic(dict){
-	for(var e in dict){
-		console.log(e);
+function __register_socket(username, socket){
+	if( !clients[username] || !clients[username].connected ){
+		clients[username] = socket;
 	}
 }
 
-function check_fomat(msg){
+function _broadcast_login(msg, socket){
+	io_server.emit('chat message', msg.from + ' is online');
+}
+
+function _send_message(msg, socket){
+	if(msg.to == 'all'){
+		io_server.emit('chat message', msg);
+	}else{
+		if((msg.to in clients) && (clients[msg.to].connected)){
+			clients[msg.to].emit('chat message', msg);
+		}else{
+			socket.emit('chat message', {'warning':'person offline'});
+		}
+	}
+}
+
+function msg_handler(msg, socket, func, throws){
+	if(__check_format(msg)){
+		__register_socket(msg.from, socket);
+		func(msg, socket);
+	}else{
+		socket.emit('chat message', {'error':'unformatted'});
+	}
+}
+
+function __check_format(msg){
 	/*
 	 * check if this message is well formatted.
 	 * a message must have keys: event, msg.data.from
@@ -56,4 +66,5 @@ function check_fomat(msg){
 	});
 
 	return formatted;
-}
+}	
+
