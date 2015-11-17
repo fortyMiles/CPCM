42da7b9b90da94e2afc72623768a04856bce4eca
fortyMiles <mqgao@outlook.com>
2015-11-17 15:36:01 +0800

    add test package

---
+++ pusher/test/socket.js
@@ -0,0 +1,18 @@
+/*
+ *
+ * A very simple socket for functional test.
+ *
+ * @author Minchiuan Gao <minchiuan.gao@gmail.com>
+ * Date: 2015-Nov-27-Tue
+ *
+ */
+
+module.exports = Socket;
+
+function Socket(){
+}
+
+Socket.prototype.emit = function(event, msg){
+	console.log('emit event: ' + event);
+	console.log('emit msg: ' + msg);
+};
